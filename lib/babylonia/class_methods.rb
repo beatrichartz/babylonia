b7b959fe51283d92f50b016e6963b48852c6e916
beatrichartz <beat.richartz@gmail.com>
2013-11-21 11:32:10 +0000

    . code cleanup

--- lib/babylonia/class_methods.rb
+++ lib/babylonia/class_methods.rb
@@ -11,72 +11,85 @@ module Babylonia
   # Class methods to extend a class with in order to make it able to handle translatable attributes
   #
   module ClassMethods
-    # Main class method ob Babylonia. Use to make attributes translatable
-    # @param [Symbol] fields the attributes to translate
-    # @param [Hash] options The options for translation
-    # @option [Symbol, Proc, String] locale The current locale - can be either a symbol that will be sent to the instance, a locale symbol that is included in available_locales or a proc that will get called with the instance. Defaults to I18n.locale at the time of use
-    # @option [Symbol, Proc, String] default_locale The fallback / default locale - can be either a symbol that will be sent to the instance, a locale symbol that is included in available_locales, a proc that will get called with the instance
-    # @option [Symbol, Proc, Array] available_locales The available locales - can be either a symbol that will be sent to the instance, a proc that will get called with the instance, or an Array of symbols of available locales. Defaults to I18n.available_locales at the time of use.
-    # @option [Boolean] fallback Wheter to fallback to the default locale or not
-    # @option [String] placeholder The placeholder to use for missing translations
-    #
-    def build_babylonian_tower_on(*fields)
-      options                     = fields.last.is_a?(Hash) ? fields.pop : {}
-      options[:locale]            ||= lambda { |r| I18n.locale }
-      options[:default_locale]    ||= lambda { |r| I18n.default_locale }
-      options[:available_locales] ||= lambda { |r| I18n.available_locales }
-      options[:fallback]          = true if options[:fallback].nil?
-      
-      fields.each do |field|
-        # Alias method chain the field to a translated value
-        # @param [Symbol] locale Pass a locale to get the field translation in this specific locale
-        # @param [Boolean] fallback Whether a fallback should be used, defaults to true
-        # @return [String, NilClass] Either the string with the translation, the fallback, the placeholder or nil
-        # @example Call a field in italian
-        #   your_instance.field(:it) #=> "Translation" 
-        #
-        define_method :"#{field}_translated" do |l=nil, options={fallback: true}|
-          field_hash    = send(:"#{field}_hash")
-          translation   = field_hash[l || locale]
-          translation   = field_hash[default_locale] if translation.nil? or translation.empty? and options[:fallback] and locale_fallback?
-          (translation.nil? or translation.empty?) ? missing_translation_placeholder(field) : translation
-        end
-        alias_method :"#{field}_raw", field
-        alias_method field, :"#{field}_translated"
-        
-        # Return the translated values as a hash
-        # @return [Hash] The hash with all the translations stored in the field
-        #
-        define_method :"#{field}_hash" do
-          field_content = send(:"#{field}_raw")
-          field_content.is_a?(String) ? YAML.load(field_content) : {}
-        end
-        
-        # Set the field to a value. This either takes a string or a hash
-        # If given a String, the current locale is set to this value
-        # If given a Hash, the hash is merged into the current translation hash, and empty values are purged
-        # @param [String, Hash] data The data to set either the current language translation or all translations to
-        # @example Set the translation for the current locale
-        #   your_object.field = "TRANSLATION"
-        # @example Set the translation and delete italian
-        #   your_object.field = {de: 'DEUTSCH', it: ''}
-        #
-        define_method :"#{field}_translated=" do |data|
-          current_hash = send(:"#{field}_hash")
-          
-          if data.is_a?(String)
-            current_hash.merge! locale => data
-          elsif data.is_a?(Hash)
-            data.delete_if{|k,v| !has_available_locale?(k) }
-            current_hash.merge! data
-          end
-          
-          send(:"#{field}_raw=", YAML.dump(current_hash.delete_if{|k,v| v.nil? or v.empty? }))
-        end
-        alias_method :"#{field}_raw=", :"#{field}="
-        alias_method :"#{field}=", :"#{field}_translated="
+    private
+    
+    def install_locale_field_getter_for(field)
+      # Alias method chain the field to a translated value
+      # @param [Symbol] locale Pass a locale to get the field translation in this specific locale
+      # @param [Boolean] fallback Whether a fallback should be used, defaults to true
+      # @return [String, NilClass] Either the string with the translation, the fallback, the placeholder or nil
+      # @example Call a field in italian
+      #   your_instance.field(:it) #=> "Translation" 
+      #
+      define_method :"#{field}_translated" do |l=nil, options={fallback: true}|
+        field_hash    = send(:"#{field}_hash")
+        translation   = field_hash[l || locale]
+        translation   = field_hash[default_locale] if translation.nil? or translation.empty? and options[:fallback] and locale_fallback?
+        (translation.nil? or translation.empty?) ? missing_translation_placeholder(field) : translation
       end
-      
+      alias_method :"#{field}_raw", field
+      alias_method field, :"#{field}_translated"
+    end
+    
+    def install_locale_field_hash_for(field)
+      # Return the translated values as a hash
+      # @return [Hash] The hash with all the translations stored in the field
+      #
+      define_method :"#{field}_hash" do
+        field_content = send(:"#{field}_raw")
+        field_content.is_a?(String) ? YAML.load(field_content) : {}
+      end
+    end
+    
+    def install_locale_field_setter_for(field)
+      # Set the field to a value. This either takes a string or a hash
+      # If given a String, the current locale is set to this value
+      # If given a Hash, the hash is merged into the current translation hash, and empty values are purged
+      # @param [String, Hash] data The data to set either the current language translation or all translations to
+      # @example Set the translation for the current locale
+      #   your_object.field = "TRANSLATION"
+      # @example Set the translation and delete italian
+      #   your_object.field = {de: 'DEUTSCH', it: ''}
+      #
+      define_method :"#{field}_translated=" do |data|
+        current_hash = send(:"#{field}_hash")
+        
+        if data.is_a?(String)
+          current_hash.merge! locale => data
+        elsif data.is_a?(Hash)
+          data.delete_if{|k,v| !has_available_locale?(k) }
+          current_hash.merge! data
+        end
+        
+        send(:"#{field}_raw=", YAML.dump(current_hash.delete_if{|k,v| v.nil? or v.empty? }))
+      end
+      alias_method :"#{field}_raw=", :"#{field}="
+      alias_method :"#{field}=", :"#{field}_translated="
+    end
+    
+    def install_virtual_locale_attributes_via_method_missing
+      # Define method missing to be able to access a language directly
+      # Enables to call a language virtual attribute directly
+      # @note Since the virtual attribute is called directly, there is no fallback on this unless you set it to true
+      # @example Call a getter directly
+      #   object.field_de #=> 'DEUTSCH'
+      # @example Call a setter directly
+      #   object.field_de = 'DEUTSCH'
+      # @example Call an untranslated field
+      #   object.field_it #=> nil
+      #
+      # @example Call a field with fallback
+      #   object.field_it(fallback: true)
+      define_method :method_missing do |meth, *args, &block|
+        if (m = meth.to_s.match(/\A([^_]+)_(\w+)(=)?\z/).to_a[1..3]) && localized?(m[0]) && has_available_locale?(m[1])
+          m[2] ? send(m[0] + m[2].to_s, {m[1].to_sym => args.first}) : send(m[0], m[1].to_sym, args.first || {})
+        else
+          super(meth, *args, &block)
+        end
+      end
+    end
+    
+    def install_locale_helpers_for(fields)
       # Return the currently active locale for the object
       # @return [Symbol] The currently active locale
       #
@@ -140,27 +153,9 @@ module Babylonia
       define_method :localized? do |attr|
         fields.include?(attr.to_sym)
       end
-      
-      # Define method missing to be able to access a language directly
-      # Enables to call a language virtual attribute directly
-      # @note Since the virtual attribute is called directly, there is no fallback on this unless you set it to true
-      # @example Call a getter directly
-      #   object.field_de #=> 'DEUTSCH'
-      # @example Call a setter directly
-      #   object.field_de = 'DEUTSCH'
-      # @example Call an untranslated field
-      #   object.field_it #=> nil
-      #
-      # @example Call a field with fallback
-      #   object.field_it(fallback: true)
-      define_method :method_missing do |meth, *args, &block|
-        if (m = meth.to_s.match(/\A([^_]+)_(\w+)(=)?\z/).to_a[1..3]) && localized?(m[0]) && has_available_locale?(m[1])
-          m[2] ? send(m[0] + m[2].to_s, {m[1].to_sym => args.first}) : send(m[0], m[1].to_sym, args.first || {})
-        else
-          super(meth, *args, &block)
-        end
-      end
-      
+    end
+    
+    def install_locale_options_evaluation(options)
       # Evaluate a localization option
       #
       define_method :evaluate_localization_option! do |option, field=nil, recursion=false|
@@ -175,6 +170,40 @@ module Babylonia
           o
         end
       end
+    end
+    
+    def evaluate_locale_options_for(fields)
+      options                     = fields.last.is_a?(Hash) ? fields.pop : {}
+      options[:locale]            ||= lambda { |r| I18n.locale }
+      options[:default_locale]    ||= lambda { |r| I18n.default_locale }
+      options[:available_locales] ||= lambda { |r| I18n.available_locales }
+      options[:fallback]          = true if options[:fallback].nil?
+      options
+    end
+    
+    # Main class method ob Babylonia. Use to make attributes translatable
+    # @param [Symbol] fields the attributes to translate
+    # @param [Hash] options The options for translation
+    # @option [Symbol, Proc, String] locale The current locale - can be either a symbol that will be sent to the instance, a locale symbol that is included in available_locales or a proc that will get called with the instance. Defaults to I18n.locale at the time of use
+    # @option [Symbol, Proc, String] default_locale The fallback / default locale - can be either a symbol that will be sent to the instance, a locale symbol that is included in available_locales, a proc that will get called with the instance
+    # @option [Symbol, Proc, Array] available_locales The available locales - can be either a symbol that will be sent to the instance, a proc that will get called with the instance, or an Array of symbols of available locales. Defaults to I18n.available_locales at the time of use.
+    # @option [Boolean] fallback Wheter to fallback to the default locale or not
+    # @option [String] placeholder The placeholder to use for missing translations
+    #
+    public
+    
+    def build_babylonian_tower_on(*fields)
+      options = evaluate_locale_options_for(fields)
+      
+      fields.each do |field|
+        install_locale_field_getter_for(field)
+        install_locale_field_setter_for(field)
+        install_locale_field_hash_for(field)
+      end
+      
+      install_locale_helpers_for(fields)
+      install_virtual_locale_attributes_via_method_missing
+      install_locale_options_evaluation(options)
     end
   end
 end
